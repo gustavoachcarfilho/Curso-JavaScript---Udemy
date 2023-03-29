b200c24ec5d9944a89f29af9fffba3caeb1ff522
Gustavo Achcar Filho <gustavofilho@greal.com.br>
2023-03-29 07:44:37 -0300

    Feitos do dia 28/03 no estágio

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,13 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
 const currencies = new Map([
   ["USD", "United States dollar"],
   ["EUR", "Euro"],
   ["GBP", "Pound sterling"],
 ]);
+*/
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -162,3 +164,25 @@ movements.forEach(function (movement, index, array) {
 
 //0: function(200)
 //1: function
+
+/*
+145. forEach With Maps and Sets
+*/
+
+//Map
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${_}: ${value}`);
+});
